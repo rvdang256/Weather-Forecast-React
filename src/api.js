f3f1980ed5d58e515cb63ba0f43eed98d8c88e7a
rvdang256 <rvd5465@psu.edu>
2024-06-11 15:37:30 -0400

    initalized project

---
+++ src/api.js
@@ -0,0 +1,17 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'f444a7a15fmshf34e2ddf0108b86p114cabjsn88bcab9ad8e4',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = '6d0a47595dd762a0a5c800dd3208e088';
+
+
